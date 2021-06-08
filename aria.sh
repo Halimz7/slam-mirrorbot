98d156bb31dc6d641e6f26f5f186a56cf26d44c7
Halimz7 <84367813+Halimz7@users.noreply.github.com>
2021-06-08 13:26:08 +0700

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -5,11 +5,11 @@ export MAX_CONCURRENT_DOWNLOADS=8
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false \
    --max-connection-per-server=13 --rpc-max-request-size=1024M \
    --bt-tracker="[$tracker_list]" --bt-max-peers=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --min-split-size=10M \
-   --follow-torrent=mem --split=10 \
+   --follow-torrent=true --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
-   --peer-id-prefix=-TR3000- --peer-agent=Transmission/3.00\
-   --disk-cache=64M --file-allocation=prealloc --continue=true --bt-request-peer-speed-limit=2048K\
+   --peer-id-prefix=-TR3000- --peer-agent=Transmission/3.00  --http -want -digest=true \
+   --disk-cache=32M --file-allocation=prealloc --continue=true --bt-request-peer-speed-limit=2048K\
    --max-file-not-found=3 --max-tries=25 --auto-file-renaming=true \
    --bt-enable-lpd=true --seed-time=0.01 --seed-ratio=1.0 \
    --content-disposition-default-utf8=true --http-accept-gzip=true --reuse-uri=true
